dc4096be66e48b069adad2b44dc7aea68085d425
mle <malaury.lemaitresalmon@gmail.com>
2022-12-06 10:55:11 +0100

    day6

---
+++ script/day6.py
@@ -0,0 +1,17 @@
+def parse_file(file):
+    with open(file) as f:
+        data = f.read()
+        return data
+
+
+def resolution(data, nb):
+    for i in range(len(data)):
+        l = [data[i] for i in range(i, i+nb)]
+        if len(l) == len(set(l)):
+            return i+nb
+
+
+x = parse_file('../data/day6.txt')
+
+print('Part one : ', resolution(x,4))
+print('Part one : ', resolution(x,14))
